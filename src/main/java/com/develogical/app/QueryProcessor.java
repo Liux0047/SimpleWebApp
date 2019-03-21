e1eb8f5b9294579d35eab958a0f2a01ec6bf2dd5
Jiazhen <fdf515@gmail.com>
2019-03-21 15:31:31 +0000

    Another change

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -8,7 +8,8 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
         if (query.toLowerCase().contains("Yaoming")) {
-            return "Chinese Basketball Player ";
+            return "Chinese Basketball Player " +
+                     "And something";
         }
         return "";
     }
