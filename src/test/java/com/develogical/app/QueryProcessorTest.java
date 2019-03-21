b5cb70d7fa18b70c0229b3a79745d9c72e9ca828
Jiazhen <fdf515@gmail.com>
2019-03-21 15:41:20 +0000

    fail

--- src/test/java/com/develogical/app/QueryProcessorTest.java
+++ src/test/java/com/develogical/app/QueryProcessorTest.java
@@ -23,4 +23,9 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+
+    @Test
+    public void testYaoMing() {
+        assertThat(queryProcessor.process("Yaoming"), containsString("Basketbal"));
+    }
 }
